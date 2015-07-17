f533be3e8098c39cb7946e5da9435b29929b5024
Khôi Tran <kt@compile.ch>
2015-07-17 16:08:00 +0200

    RC1

--- README.md
+++ README.md
@@ -10,5 +10,44 @@ Plugin has been tested on Redmine 2.4.x and 2.6.x. But should be compatible with
     git clone https://github.com/KeeTraxx/redmine-timereporter.git
     
 ## Configure
-The REST endpoint can be configured in the plugin configuration screen.W
+The REST endpoint can be configured in the plugin configuration screen.
 
+## Project custom fields
+
+## REST backend format
+The plugin will HTTP POST to the REST backend in this JSON format:
+ 
+    {
+        "user": {
+            "login": "admin",
+            "mail": "admin@example.net",
+            "firstname": "Redmine",
+            "lastname": "Admin",
+            "id": 1
+        },
+        "issue_id": "1",
+        "project_id": 1,
+        "user_id": 1,
+        "hours": 1,
+        "time_entry": {
+            "comments": "1",
+            "hours": 1,
+            "issue_id": 1,
+            "project_id": 1,
+            "tmonth": 7,
+            "tweek": 29,
+            "tyear": 2015
+        },
+        "project": {
+            "description": "Testerproject",
+            "homepage": "",
+            "id": 1,
+            "identifier": "test",
+            "name": "Test",
+            "parent_id": null,
+            "status": 1,
+            "custom_fields": {
+                "backend_id": "2"
+            }
+        }
+    }
--- app/models/reporter.rb
+++ app/models/reporter.rb
@@ -7,34 +7,56 @@ class Reporter < ActiveRecord::Base
   # Should be called by the Redmine Hook "controller_timelog_edit_before_save"
   def self.send_time_log(user, params, time_entry)
 
-
-    #Rails.logger.warn( CustomField.find(:all) )
+    # Basic validation, because there is no Hook named "controller_timelog_edit_after_save"
+    if params["time_entry"]["spent_on"].blank? || params["time_entry"]["hours"].blank? || params["time_entry"]["activity_id"].blank?
+      return
+    end
 
     project = Project.find(time_entry[:project_id])
+    issue = Issue.find(params[:issue_id])
+
+    Rails.logger.warn(issue.inspect)
+
     p = pick(project, [:description, :homepage, :id, :identifier, :name, :parent_id, :status])
     p[:custom_fields] = {}
     project.custom_field_values.each do |custom_field|
-      #Rails.logger.warn("blka")
-      #Rails.logger.warn(custom_field)
-      #Rails.logger.warn(custom_field.inspect)
-      #Rails.logger.warn(custom_field.methods(true))
-      #Rails.logger.warn(custom_field.custom_field)
       p[:custom_fields][custom_field.custom_field[:name]] = custom_field.value
-      #name = custom_field[:custom_field][:name]
-      #p[:custom_fields][name] = custom_field[:value]
     end
 
     #Rails.logger.warn(project.custom_field_values)
 
     data = {
-        :user => pick(user, [:login, :email, :firstname, :lastname, :id]),
+        :user => pick(user, [:login, :mail, :firstname, :lastname, :id]),
         :issue_id => params[:issue_id],
         :project_id => time_entry[:project_id],
         :user_id => user[:id],
         :hours => time_entry[:hours],
         :time_entry => pick(time_entry, [:comments, :hours, :issue_id, :project_id, :tmonth, :tweek, :tyear]),
-        :project => p
+        :project => p,
+        :issue => pick(issue, [
+                                :tracker_id,
+                                :project_id,
+                                :subject,
+                                :description,
+                                :due_date,
+                                :category_id,
+                                :status_id,
+                                :assigned_to_id,
+                                :priority_id,
+                                :fixed_version_id,
+                                :author_id,
+                                :created_on,
+                                :updated_on,
+                                :start_date,
+                                :done_ratio,
+                                :estimated_hours,
+                                :parent_id,
+                                :root_id,
+                                :closed_on
+                            ])
     }
+
+    Rails.logger.warn(data)
 
     post_to_server(data)
   end
@@ -48,7 +70,6 @@ class Reporter < ActiveRecord::Base
                        "value" => j.value
                    })
     end
-    u = flatten_user(user)
     post_to_server({
                        "type" => "issue",
                        "user" => u.to_json,
